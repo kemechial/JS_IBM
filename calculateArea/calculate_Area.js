17626409f1706e89661a5503a230c46c39167e31
kemechial <kaanevran@hotmail.com>
2024-11-17 10:46:42 -0500

    groceryTracker function added.

--- calculateArea/calculate_Area.js
+++ calculateArea/calculate_Area.js
@@ -10,3 +10,18 @@ function calculateArea() {
   document.getElementById('result').innerText = `The area of the rectangle is: ${area}`;
 
 }
+
+let p1;
+let p2;
+let p3;
+
+function groceryTracker() {
+    p1 = parseFloat(document.getElementById('price1').value);
+    p2 = parseFloat(document.getElementById('price2').value);
+    p3 = parseFloat(document.getElementById('price3').value);
+
+    let total = p1 + p2 + p3;
+
+    document.getElementById('summation').innerText = `total sum is: ${total}`;
+
+}
